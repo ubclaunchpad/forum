992a02d8f0d84d92a2111573d1225f5114be9a99
xenshreyas <xenshreyas@gmail.com>
2024-10-05 12:49:31 -0700

    add uvicorn to run backend

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
-items = []
+
 @app.get("/")
 def root():
-    return {"message": "Hello"}
+    return {"message": "Hello from the backend!"}
 
-@app.post("/items")
-def create_item(item: str):
-    items.append(item)
-    return {"message" : items}
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
