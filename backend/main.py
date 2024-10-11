555430fa2fbe014ea8324e4915647d365e78e8bd
Arman Moztarzadeh <armanmoztar@outlook.com>
2024-10-11 15:48:40 -0700

    style: run black formatting

--- backend/main.py
+++ backend/main.py
@@ -3,9 +3,11 @@ import uvicorn
 
 app = FastAPI()
 
+
 @app.get("/")
 def root():
     return {"message": "Hello from the backend!"}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
