bd7fc154f50336fd89f905c61fdaf0de43941400
Ray Zhou <rayzhou4@gmail.com>
2024-10-22 14:46:41 -0700

    delete unnecessary auth router + create auth middleware

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,6 @@ import uvicorn
 from dotenv import load_dotenv
 import os
 from supabase import create_client, Client
-from routers.auth import router as auth_router
 from middleware.auth import AuthMiddleware
 
 load_dotenv()
@@ -11,7 +10,6 @@ load_dotenv()
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
-
 
 app = FastAPI()
 
@@ -21,15 +19,15 @@ AUTH_MIDDLEWARE_ENABLED = True
 PUBLIC_PATHS = ['/']
 
 # middlewares
-app.add_middleware(AuthMiddleware, enabled=AUTH_MIDDLEWARE_ENABLED, exempt_paths=PUBLIC_PATHS)
+app.add_middleware(AuthMiddleware, enabled=AUTH_MIDDLEWARE_ENABLED, public_paths=PUBLIC_PATHS)
 
 @app.get("/")
 def root():
     return {"message": "Hello from the backend!"}
 
-app.include_router(
-    auth_router, prefix="/auth", dependencies=[Depends(lambda: supabase)]
-)
+@app.get("/protected")
+def filler_protected_path(): # NOTE: delete later, used for testing
+    return {"message": "Protect route!"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- backend/middleware/auth.py
+++ backend/middleware/auth.py
@@ -1,22 +1,31 @@
 from fastapi import Request, Response
 from starlette.middleware.base import BaseHTTPMiddleware
+from dotenv import load_dotenv
+import os
+from supabase import create_client, Client
+
+load_dotenv()
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
 class AuthMiddleware(BaseHTTPMiddleware):
-    def __init__(self, app, enabled: bool = True, exempt_paths: list = None):
+    def __init__(self, app, enabled: bool = True, public_paths: list = None):
         super().__init__(app)
         self.enabled = enabled
-        self.exempt_paths = exempt_paths
+        self.public_paths = public_paths
 
     async def dispatch(self, request: Request, call_next):
         # Check if the middleware is enabled
         if self.enabled:
-            # Skip authentication for exempt paths
-            if request.url.path in self.exempt_paths:
+            # Skip authentication for public paths
+            if request.url.path in self.public_paths:
                 return await call_next(request)
             
-            # Middleware logic goes here (e.g., authentication checks) !!!TODO!!!
-            token = request.headers.get("Authorization")
-            if not token:
+            # checks if user is authenticated / authorized
+            user = supabase.auth.get_user()
+            if not user:
                 return Response("Unauthorized", status_code=401)
 
         # Proceed to the next middleware or endpoint
