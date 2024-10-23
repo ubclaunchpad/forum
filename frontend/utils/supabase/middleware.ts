2472697bc88d359126908e188fbae29aca74bba3
malcolm <malcolmmackenzie20@gmail.com>
2024-10-22 23:49:13 -0700

    add root to urls that don't need authentication

--- frontend/utils/supabase/middleware.ts
+++ frontend/utils/supabase/middleware.ts
@@ -37,6 +37,18 @@ export async function updateSession(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser();
 
+  if (
+    !user &&
+    !request.nextUrl.pathname.startsWith('/signin') &&
+    !request.nextUrl.pathname.startsWith('/auth') &&
+    request.nextUrl.pathname !== '/'
+  ) {
+    // no user, potentially respond by redirecting the user to the login page
+    const url = request.nextUrl.clone()
+    url.pathname = '/signin'
+    return NextResponse.redirect(url)
+  }
+
   // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
   // creating a new response object with NextResponse.next() make sure to:
   // 1. Pass the request in it, like so:
