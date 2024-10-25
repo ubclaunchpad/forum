94daf14b1e6409d9d2901d378cd58b9099757040
Arman Moztarzadeh <armanmoztar@outlook.com>
2024-10-25 13:53:57 -0700

    remove unused variables

--- frontend/src/utils/supabase/middleware.ts
+++ frontend/src/utils/supabase/middleware.ts
@@ -15,7 +15,7 @@ export async function updateSession(request: NextRequest) {
           return request.cookies.getAll();
         },
         setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) =>
+          cookiesToSet.forEach(({ name, value }) =>
             request.cookies.set(name, value),
           );
           supabaseResponse = NextResponse.next({
