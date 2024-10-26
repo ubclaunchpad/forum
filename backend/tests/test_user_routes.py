606158b9d550790c479352a76fd41d4318b3c70d
Boris Wang <boris.wang117@gmail.com>
2024-10-25 20:17:27 -0700

    Add tests in case of invalid user or invalid credentials

--- backend/tests/test_user_routes.py
+++ backend/tests/test_user_routes.py
@@ -3,13 +3,30 @@ from routers.routes.user_routes import user_router
 
 client = TestClient(user_router)
 
-email = ""
-user_id = ""
+email = "test"
+user_id = "test"
 
 
 def test_get_profile():
     response = client.get(f"/users/me")
+    print(response)
     assert response.status_code == 200
+
+
+def test_get_profile_not_found():
+    header = {}
+    response = client.get(f"/users/me")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found."}
+
+
+def test_get_profile_invalid_creds():
+    invalid_header = {}
+    response = client.get(f"/users/me")
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "You do not have permission to access this profile."
+    }
 
 
 def test_update_profile():
@@ -17,9 +34,41 @@ def test_update_profile():
     assert response.status_code == 200
 
 
+def test_update_profile_not_found():
+    header = {}
+    response = client.post(f"/users/me")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found."}
+
+
+def test_update_profile_invalid_creds():
+    invalid_header = {}
+    response = client.post(f"/users/me")
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "You do not have permission to access this profile."
+    }
+
+
 def test_delete_profile():
     response = client.delete(f"/users/me")
     assert response.status_code == 200
+
+
+def test_delete_profile_not_found():
+    header = {}
+    response = client.delete(f"/users/me")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found."}
+
+
+def test_delete_profile_invalid_creds():
+    invalid_header = {}
+    response = client.delete(f"/users/me")
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "You do not have permission to access this profile."
+    }
 
 
 def test_get_all_users():
@@ -27,9 +76,31 @@ def test_get_all_users():
     assert response.status_code == 200
 
 
+def test_get_all_users_invalid_cred():
+    response = client.get("/users")
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "You do not have permission to perform this action."
+    }
+
+
 def test_get_user_by_id():
     response = client.get(f"/users/{user_id}")
     assert response.status_code == 200
+
+
+def test_get_user_by_id_not_found():
+    response = client.get(f"/users/{user_id}")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found."}
+
+
+def test_get_user_by_id_invalid_creds():
+    response = client.get(f"/users/{user_id}")
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "You do not have permission to perform this action."
+    }
 
 
 def test_update_user_by_id():
@@ -37,9 +108,45 @@ def test_update_user_by_id():
     assert response.status_code == 200
 
 
+def test_update_user_by_id_not_found():
+    response = client.post(f"/users/{user_id}")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found."}
+
+
+def test_update_user_by_id_invalid_creds():
+    response = client.post(f"/users/{user_id}")
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "You do not have permission to perform this action."
+    }
+
+
 def test_delete_user_by_id():
     response = client.delete(f"/users/{user_id}")
     assert response.status_code == 200
+
+
+def test_delete_user_by_id_not_found():
+    response = client.delete(f"/users/{user_id}")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found."}
+
+
+def test_delete_user_by_id_invalid_creds():
+    response = client.delete(f"/users/{user_id}")
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "You do not have permission to perform this action."
+    }
+
+
+def test_delete_user_by_id_not_found():
+    response = client.delete(f"/users/{user_id}")
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "You do not have permission to perform this action."
+    }
 
 
 def test_get_user_by_email():
@@ -47,11 +154,53 @@ def test_get_user_by_email():
     assert response.status_code == 200
 
 
+def test_get_user_by_email_not_found():
+    response = client.get(f"/users/{email}")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found."}
+
+
+def test_get_user_by_email_invalid_creds():
+    response = client.get(f"/users/{email}")
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "You do not have permission to perform this action."
+    }
+
+
 def test_update_user_by_email():
     response = client.post(f"/users/{email}")
     assert response.status_code == 200
 
 
-def test_delete_user_by_email():
+def test_update_user_by_email_not_found():
     response = client.post(f"/users/{email}")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found."}
+
+
+def test_update_user_by_email_invalid_creds():
+    response = client.post(f"/users/{email}")
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "You do not have permission to perform this action."
+    }
+
+
+def test_delete_user_by_email():
+    response = client.delete(f"/users/{email}")
     assert response.status_code == 200
+
+
+def test_delete_user_by_email_not_found():
+    response = client.delete(f"/users/{email}")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found."}
+
+
+def test_delete_user_by_email_invalid_creds():
+    response = client.delete(f"/users/{email}")
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "You do not have permission to perform this action."
+    }
