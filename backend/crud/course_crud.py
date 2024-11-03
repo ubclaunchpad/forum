44252d6fc0686029994d7475d798953e3996a85b
olicheng <olicheng@student.ubc.ca>
2024-11-03 15:14:40 -0800

    basic crud for courses

--- backend/crud/course_crud.py
+++ backend/crud/course_crud.py
@@ -1,13 +1,40 @@
+import uuid
+
 from ..database.db import supabase
 
 
 def register_course(course):
     return None
 
+def get_courses():
+    return supabase.table("courses").select("*")
 
-def update_course(course):
-    return None
+def get_course_by_id(course_id: uuid):
+    return supabase.table("course").select("*").eq('id', course_id).execute()
+
+def update_course(course_id: uuid, name: str = None, max_users: int = None):
+    update_data = {}
+    if name is not None:
+        update_data["name"] = name
+    if max_users is not None:
+        update_data["max_users"] = max_users
+
+    if not update_data:  # no updates to do, can return early
+        return
+
+    response = supabase.table("courses").update(update_data).eq('id', course_id)
+    return response
 
 
-def delete_course(course):
-    return None
+def delete_course(course_id: uuid):
+    response = supabase.table("courses").delete().eq('id', course_id)
+    return response
+
+
+def create_course(c_group: str, c_code: str, term: str):
+    response = supabase.table("courses").insert({
+        "c_group": c_group,
+        "c_code": c_code,
+        "term": term
+    }).execute()
+    return response
--- backend/routers/routes/courses.py
+++ backend/routers/routes/courses.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, Request, Response
 
 from backend.crud import user_crud
+from backend.database.db import supabase
 
 course_router = APIRouter()
 
@@ -13,3 +14,20 @@ async def register_course():
         raise HTTPException(status_code=404, detail="Failed to create course.")
 
     return profile
+
+@course_router.get("/courses/page")
+async def get_courses():
+    return
+
+@course_router.post("/courses/create")
+async def create_course(c_group: str, c_code: str, term: str):
+    return
+
+@course_router.delete("/courses/delete")
+async def delete_course():
+    return
+
+@course_router.put("/courses/update")
+async def update_course():
+    return
+
