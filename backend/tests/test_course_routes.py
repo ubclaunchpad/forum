2feff277214ed47e8574681ac9920c9878cafe75
olicheng <olicheng@student.ubc.ca>
2024-11-05 15:23:58 -0800

    updated test_course_routes

--- backend/tests/test_course_routes.py
+++ backend/tests/test_course_routes.py
@@ -1,77 +1,70 @@
 from fastapi.testclient import TestClient
 
-from backend.routers.routes.courses import course_router
+from backend.main import app
 
-client = TestClient(course_router)
+INSERT_ENDPOINT = "/courses/create"
+UPDATE_ENDPOINT = "/courses/update"
+GET_ENDPOINT = "/courses/page"
+GET_ID_ENDPOINT = "/courses"
+DELETE_ENDPOINT = "/courses/delete"
+REGISTER_ENDPOINT = "/courses/register"
+
+client = TestClient(app)
 
 email = "test"
 course_id = "test"
 
+def test_insert_course():
+    test_course = {
+        "c_group": "test_group",
+        "c_code": "test_code",
+        "term": "test_term"
+    }
+    response = client.post(INSERT_ENDPOINT, json=test_course)
+    assert response.status_code == 200
+
+def test_insert_duplicate_course():
+    header = {}
+    response = client.post(INSERT_ENDPOINT)
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Course not found."}
+
+def test_insert_course_invalid_creds():
+    invalid_header = {}
+    response = client.post(INSERT_ENDPOINT)
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "You do not have permission to access this course."
+    }
 
 def test_get_courses():
-    response = client.get(f"/courses/me")
+    response = client.get(GET_ENDPOINT)
     assert response.status_code == 200
 
 def test_get_courses_by_id():
-    response = client.get(f"/courses/me")
+    response = client.get(GET_ENDPOINT)
     assert response.status_code == 200
 
 def test_get_courses_not_found():
     header = {}
-    response = client.get(f"/courses/me")
+    response = client.get(GET_ENDPOINT)
     assert response.status_code == 404
     assert response.json() == {"detail": "Course not found."}
 
 def test_get_courses_invalid_creds():
     invalid_header = {}
-    response = client.get(f"/courses/me")
-    assert response.status_code == 403
-    assert response.json() == {
-        "detail": "You do not have permission to access this course."
-    }
-
-def test_update_course():
-    response = client.post(f"/courses/me")
-    assert response.status_code == 200
-
-def test_update_course_not_found():
-    header = {}
-    response = client.post(f"/courses/me")
-    assert response.status_code == 404
-    assert response.json() == {"detail": "Course not found."}
-
-def test_update_course_invalid_creds():
-    invalid_header = {}
-    response = client.post(f"/courses/me")
-    assert response.status_code == 403
-    assert response.json() == {
-        "detail": "You do not have permission to access this course."
-    }
-
-def test_delete_course():
-    response = client.delete(f"/courses/me")
-    assert response.status_code == 200
-
-def test_delete_course_not_found():
-    header = {}
-    response = client.delete(f"/courses/me")
-    assert response.status_code == 404
-    assert response.json() == {"detail": "Course not found."}
-
-def test_delete_course_invalid_creds():
-    invalid_header = {}
-    response = client.delete(f"/courses/me")
+    response = client.get(GET_ENDPOINT)
     assert response.status_code == 403
     assert response.json() == {
         "detail": "You do not have permission to access this course."
     }
 
 def test_get_all_courses():
-    response = client.get("/courses")
+    response = client.get(GET_ENDPOINT)
     assert response.status_code == 200
 
 def test_get_all_courses_invalid_cred():
-    response = client.get("/courses")
+    response = client.get(GET_ENDPOINT)
     assert response.status_code == 403
     assert response.json() == {
         "detail": "You do not have permission to perform this action."
@@ -94,32 +87,32 @@ def test_get_course_by_id_invalid_creds():
     }
 
 def test_update_course_by_id():
-    response = client.post(f"/courses/{course_id}")
+    response = client.put(UPDATE_ENDPOINT)
     assert response.status_code == 200
 
 def test_update_course_by_id_not_found():
-    response = client.post(f"/courses/{course_id}")
+    response = client.put(UPDATE_ENDPOINT)
     assert response.status_code == 404
     assert response.json() == {"detail": "Course not found."}
 
 def test_update_course_by_id_invalid_creds():
-    response = client.post(f"/courses/{course_id}")
+    response = client.put(UPDATE_ENDPOINT)
     assert response.status_code == 403
     assert response.json() == {
         "detail": "You do not have permission to perform this action."
     }
 
 def test_delete_course_by_id():
-    response = client.delete(f"/courses/{course_id}")
+    response = client.delete(DELETE_ENDPOINT)
     assert response.status_code == 200
 
 def test_delete_course_by_id_not_found():
-    response = client.delete(f"/courses/{course_id}")
+    response = client.delete(DELETE_ENDPOINT)
     assert response.status_code == 404
     assert response.json() == {"detail": "Course not found."}
 
 def test_delete_course_by_id_invalid_creds():
-    response = client.delete(f"/courses/{course_id}")
+    response = client.delete(DELETE_ENDPOINT)
     assert response.status_code == 403
     assert response.json() == {
         "detail": "You do not have permission to perform this action."
