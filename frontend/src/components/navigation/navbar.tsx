8ed6fc4f976ee928e93ae77be94b3543ce8a49be
Armin Talaie <talaiearmin78@gmail.com>
2024-11-06 17:16:21 -0800

    setting up midfi

---
+++ frontend/src/components/navigation/navbar.tsx
@@ -0,0 +1,12 @@
+import { cn } from "@/lib/utils";
+
+type NavbarProps = {
+    children: React.ReactNode;
+    variant: "default" | "compact";
+}
+
+const navClass = "flex items-center justify-between bg-[#2F43CB] h-[56px]";
+
+export default function Navbar({ children, variant }: NavbarProps) {
+    return <nav className={cn(navClass, variant)}>{children}</nav>;
+}
