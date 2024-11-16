b53e58f5be37a735d88a4b7d548345158ba8f298
Joey Xiang <joey.xiang426@gmail.com>
2024-11-16 12:01:43 -0800

    update some styling and add publish course button

--- frontend/src/app/create-course/page.tsx
+++ frontend/src/app/create-course/page.tsx
@@ -99,62 +99,77 @@ const CreateCoursePage = () => {
     // TODO: add invite functionality
   };
 
+  const handlePublish = async () => {
+    // TODO: add publish functionality
+  };
+
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <div className="flex flex-col items-start gap-12 w-1/2 min-w-[200px]">
-        <h1 className="self-stretch">Create a course</h1>
-        <div className="flex flex-col items-start gap-[19px] self-stretch">
-          <div className="flex items-center gap-[73px] self-stretch">
-            <Form
-              title="Course Group"
-              tagName="courseGroup"
-              placeholder="e.g. CPSC"
-            />
-            <Form
-              title="Course Code"
-              tagName="courseCode"
-              placeholder="e.g. 110"
-            />
-            <Form
-              title="Course Section (optional)"
-              tagName="courseSection"
-              placeholder="e.g. 101/103"
-            />
-          </div>
-          <div className="w-1/2">
-            <Form
-              title="Course Name"
-              tagName="courseName"
-              placeholder="e.g. Computation, programs, and programming"
-            />
-          </div>
-          <div className="flex flex-col items-start gap-[30px] w-full">
-            <div className="flex flex-col items-start gap-2 self-stretch">
-              <p className="self-stretch">Add Administrators</p>
-              <div className="flex items-start gap-2 self-stretch">
-                <Input
-                  type="text"
-                  name="addAdministrators"
-                  placeholder="Add Administrators"
-                  className="bg-white text-white border-gray-600"
-                  required
-                />
-                <Button
-                  type="button"
-                  onClick={handleInvite}
-                  className="bg-blue-600 hover:bg-blue-500 text-white flex-1"
-                >
-                  Invite
-                </Button>
-              </div>
+      <div className="flex flex-col items-start gap-20 w-1/2 min-w-[200px]">
+        <div className="flex flex-col items-start gap-12 self-stretch">
+          <h1 className="self-stretch">Create a course</h1>
+          <div className="flex flex-col items-start gap-[19px] self-stretch">
+            <div className="flex items-center gap-[73px] self-stretch">
+              <Form
+                title="Course Group"
+                tagName="courseGroup"
+                placeholder="e.g. CPSC"
+              />
+              <Form
+                title="Course Code"
+                tagName="courseCode"
+                placeholder="e.g. 110"
+              />
+              <Form
+                title="Course Section (optional)"
+                tagName="courseSection"
+                placeholder="e.g. 101/103"
+              />
             </div>
-            {/* TODO: add logic for multiple profiles */}
-            <Profile
-              name="Gregor Kiczales"
-              email="email@ubc.ca"
-              pronouns="He/Him/His"
-            />
+            <div className="w-1/2">
+              <Form
+                title="Course Name"
+                tagName="courseName"
+                placeholder="e.g. Computation, programs, and programming"
+              />
+            </div>
+            <div className="flex flex-col items-start gap-[30px] w-full">
+              <div className="flex flex-col items-start gap-2 self-stretch">
+                <p className="self-stretch">Add Administrators</p>
+                <div className="flex items-start gap-2 self-stretch">
+                  <Input
+                    type="text"
+                    name="addAdministrators"
+                    placeholder="Add Administrators"
+                    className="bg-white text-white border-gray-600"
+                    required
+                  />
+                  <Button
+                    type="button"
+                    onClick={handleInvite}
+                    className="bg-blue-600 hover:bg-blue-500 text-white flex-1"
+                  >
+                    Invite
+                  </Button>
+                </div>
+              </div>
+              {/* TODO: add logic for multiple profiles */}
+              <Profile
+                name="Gregor Kiczales"
+                email="email@ubc.ca"
+                pronouns="He/Him/His"
+              />
+            </div>
           </div>
+        </div>
+        <div className="w-full flex justify-end">
+          <Button
+            type="button"
+            onClick={handlePublish}
+            className="bg-blue-600 hover:bg-blue-500 text-white"
+          >
+            Publish Course
+          </Button>
         </div>
       </div>
     </div>
