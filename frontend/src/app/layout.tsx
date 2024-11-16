e728c9520768f2e5b7d42ae67a3cc1d5dfdbd4f6
jyxng1 <jyxng1@users.noreply.github.com>
2024-11-16 21:19:17 +0000

    Prettified Code!

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,9 +3,9 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter',
-})
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "Forum",
@@ -19,11 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.variable} font-sans`}
-      >
-        {children}
-      </body>
+      <body className={`${inter.variable} font-sans`}>{children}</body>
     </html>
   );
 }
