2ddae8cf1a9848fea024fb887a386458987e3567
malcolmm20 <malcolmmackenzie20@gmail.com>
2024-11-16 11:08:51 -0800

    Malcolm profile (#68)
    * added profile page and usercontext
    
    * Prettified Code!
    
    ---------
    
    Co-authored-by: malcolmm20 <malcolmm20@users.noreply.github.com>

--- frontend/src/utils/supabase/middleware.ts
+++ frontend/src/utils/supabase/middleware.ts
@@ -39,13 +39,12 @@ export async function updateSession(request: NextRequest) {
 
   if (
     !user &&
-    !request.nextUrl.pathname.startsWith("/signin") &&
     !request.nextUrl.pathname.startsWith("/auth") &&
     request.nextUrl.pathname !== "/"
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
-    url.pathname = "/signin";
+    url.pathname = "/auth/signin";
     return NextResponse.redirect(url);
   }
 
