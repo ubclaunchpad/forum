7b6f84df6712888118292a073f604a4860f0c23b
olicheng <olicheng@student.ubc.ca>
2024-11-19 10:33:41 -0800

    black format

--- backend/routers/routes/courses.py
+++ backend/routers/routes/courses.py
@@ -1,10 +1,7 @@
 from fastapi import APIRouter, HTTPException, Request
 from backend.crud import course_crud
 from backend.crud.course_crud import UserNotEnrolledException, NoPermissionException
-from backend.routers.req.courses_req import (
-    CreateCourseReq,
-    UpdateCourseReq
-)
+from backend.routers.req.courses_req import CreateCourseReq, UpdateCourseReq
 
 course_router = APIRouter()
 course_router_endpoint = "/courses"
