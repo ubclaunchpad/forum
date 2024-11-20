f061fe1d5226bbc21470cb3fa547a7e9c56d2eb0
OliCheng <69122904+olichengus@users.noreply.github.com>
2024-11-20 13:18:16 -0800

    Course Crud endpoints (#97)
    * basic crud for courses
    
    * Fixed relative import
    
    * Register user to course
    
    * courses req classes added
    
    * updated test_course_routes
    
    * started tests for courses table
    
    * catch api errors
    
    * Update Course Req
    
    * Finished basic pytests for courses
    
    * Updated migration script
    
    * course endpoints w/o current user validation
    
    * get_courses_by_id
    
    * user added in request
    
    * implemented create course
    
    * Course crud operations
    
    * Applied black linting
    
    * rename req models
    
    * black format
    
    * arman changes
    
    * linter run

---
+++ backend/routers/req/courses_req.py
@@ -0,0 +1,30 @@
+from uuid import UUID
+from datetime import date
+from pydantic import BaseModel
+
+
+class CreateCourseReq(BaseModel):
+    id: int | None = None
+    c_group: str
+    code: str
+    section: str
+    name: str | None = None
+    config: str | None = None
+    start_date: date | None = None
+    end_date: date | None = None
+
+
+class RegisterUserReq(BaseModel):
+    c_id: int
+    u_id: UUID
+
+
+class UpdateCourseReq(BaseModel):
+    c_id: int
+    c_group: str | None = None
+    code: str | None = None
+    section: str | None = None
+    name: str | None = None
+    config: str | None = None
+    start_date: date | None = None
+    end_date: date | None = None
--- backend/routers/routes/courses.py
+++ backend/routers/routes/courses.py
@@ -1,14 +1,104 @@
-from crud import user_crud
-from fastapi import APIRouter, Depends, HTTPException, Request, Response
+from fastapi import APIRouter, HTTPException, Request
+from backend.crud import course_crud
+from backend.crud.course_crud import UserNotEnrolledException, NoPermissionException
+from backend.routers.req.courses_req import CreateCourseReq, UpdateCourseReq
 
 course_router = APIRouter()
+course_router_endpoint = "/courses"
+
+# @course_router.post(course_router_endpoint)
+# async def register_course(create_course_req: RegisterUserReq):
+#     profile = course_crud.register_course(create_course_req)
+#     if not profile:
+#         raise HTTPException(status_code=404, detail="Failed to create course.")
+#     return profile
 
 
-@course_router.post("/courses/register")
-async def register_course():
-    profile = user_crud.register_course()
+@course_router.get(course_router_endpoint)
+async def get_courses(request: Request):
+    courses = course_crud.get_courses(request.headers.get("X-User-ID"))
+    if not courses:
+        raise HTTPException(status_code=404, detail="Failed to fetch courses.")
+    return courses
 
-    if not profile:
+
+# get course by id
+@course_router.get(course_router_endpoint + "/{c_id}")
+async def get_courses_by_id(c_id: int, request: Request):
+    try:
+        courses = course_crud.get_course_by_id(c_id, request.headers.get("X-User-ID"))
+        if not courses:
+            raise HTTPException(status_code=404, detail="Failed to fetch course.")
+        return courses
+    except UserNotEnrolledException as e:
+        raise HTTPException(
+            status_code=404, detail="User is not enrolled in this course"
+        )
+
+
+@course_router.post(course_router_endpoint)
+async def create_course(create_course_req: CreateCourseReq, request: Request):
+    user_id = request.headers.get("X-User-ID")
+    course = course_crud.create_course(create_course_req)
+    if not course:
         raise HTTPException(status_code=404, detail="Failed to create course.")
+    # gets Admin Enum
+    admin_enum = course_crud.get_role_key("Admin")
+    if not admin_enum:
+        raise HTTPException(status_code=404, detail="Failed to find Admin role")
+    course_id = course.data[0]["id"]
+    admin_id = admin_enum.data[0]["id"]
+    # Adds user as admin to the added course
+    add_user = course_crud.add_user_to_course(course_id, user_id, admin_id)
+    if not add_user:
+        raise HTTPException(
+            status_code=404, detail="Failed to add user as admin to course."
+        )
+    return course
 
-    return profile
+
+@course_router.delete(course_router_endpoint + "/{c_id}")
+async def delete_course(c_id: int, request: Request):
+    try:
+        admin_enum = course_crud.get_role_key("Admin")
+        if not admin_enum:
+            raise HTTPException(status_code=404, detail="Failed to find Admin role")
+        admin_id = admin_enum.data[0]["id"]
+        response = course_crud.delete_course(
+            c_id, request.headers.get("X-User-ID"), admin_id
+        )
+        if not response:
+            raise HTTPException(status_code=404, detail="Failed to delete course.")
+        return response
+    except NoPermissionException as e:
+        raise HTTPException(
+            status_code=404, detail="User has no permission to delete role"
+        )
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+
+
+@course_router.put(course_router_endpoint)
+async def update_course(update_course_req: UpdateCourseReq, request: Request):
+    try:
+        # Get admin and maintainer role values
+        admin_enum = course_crud.get_role_key("Admin")
+        maintainer_enum = course_crud.get_role_key("Maintainer")
+        if not admin_enum or not maintainer_enum:
+            raise HTTPException(
+                status_code=404, detail="Failed to get Admin and Maintaner roles"
+            )
+        admin_id = admin_enum.data[0]["id"]
+        maintainer_id = maintainer_enum.data[0]["id"]
+        response = course_crud.update_course(
+            update_course_req, request.headers.get("X-User-ID"), admin_id, maintainer_id
+        )
+        if not response:
+            raise HTTPException(status_code=404, detail="Failed to update course.")
+        return response
+    except NoPermissionException as e:
+        raise HTTPException(
+            status_code=404, detail="User has no permission to update role"
+        )
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
