16b226603333f92235c4f317f897f4a113e74ed8
Joey Xiang <joey.xiang426@gmail.com>
2024-11-22 14:24:35 -0800

    course tabs with url persist tab state

---
+++ frontend/src/app/course-home/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import CourseNavbar from "./course-navbar";
+
+export default function CourseHomePage() {
+  return (
+    <div className="min-h-screen bg-background flex justify-center items-start">
+      <CourseNavbar />
+    </div>
+  );
+}
