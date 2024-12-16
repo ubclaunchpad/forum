b8470853af57101ab2fc91d7619e4aefc84205c4
Boris Wang <boris.wang117@gmail.com>
2024-12-16 13:15:27 -0700

    Initalize branch

--- backend/controllers/post_controller.py
+++ backend/controllers/post_controller.py
@@ -3,9 +3,14 @@ from uuid import UUID
 
 from fastapi import HTTPException
 from fastapi.encoders import jsonable_encoder
-from models.all import Post, Profile
+from models.all import Post, PostEdit, Profile
 from models.db import get_db
-from models.schemas.post_schema import CreatePostRequest
+from models.schemas.general_schema import GeneralResponse
+from models.schemas.post_schema import (
+    CreatePostEditRequest,
+    CreatePostRequest,
+    PostEditResponse,
+)
 
 
 def create_post(user_id: str, c_id: str, post_info: CreatePostRequest) -> Post:
@@ -31,4 +36,79 @@ def get_posts(c_id: str) -> List[Post]:
             return posts
     except Exception as e:
         print(f"Error in get_posts: {type(e).__name__}: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Failed to fetch posts: {str(e)}")
+
+
+def get_post(c_id: str, p_id: str) -> Post:
+    try:
+        with get_db() as db:
+            post = (
+                db.query(Post).filter(Post.course_id == c_id, Post.id == p_id).first()
+            )
+            # Get post edits
+            # Get post user-events
+            return post
+    except Exception as e:
+        print(f"Error in get_post: {type(e).__name__}: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Failed to fetch posts: {str(e)}")
+
+
+def update_post(post_edit_info: CreatePostEditRequest, c_id: str) -> PostEditResponse:
+    try:
+        with get_db() as db:
+            p_id = post_edit_info.post_id
+            post = (
+                db.query(Post).filter(Post.course_id == c_id, Post.id == p_id).first()
+            )
+
+            if not post:
+                raise HTTPException(status_code=404, detail="Post not found")
+
+            old_content = post.content
+
+            if post_edit_info.new_content == old_content:
+                raise HTTPException(status_code=400, detail="Content is duplicate")
+
+            # Type error, needs fix
+            post.content = post_edit_info.new_content
+
+            post_edit = PostEdit(
+                post_id=post_edit_info.post_id,
+                edited_by=post_edit_info.edited_by,
+                previous_content=post_edit_info.previous_content,
+                new_content=post_edit_info.new_content,
+                edit_reason=post_edit_info.edit_reason,
+            )
+
+            db.add(post_edit)
+            db.flush()
+            id = UUID(str(post_edit.id))
+            return PostEditResponse(
+                id=id,
+                post_id=post_edit_info.post_id,
+                edited_by=post_edit_info.edited_by,
+                previous_content=post_edit_info.previous_content,
+                new_content=post_edit_info.new_content,
+                edit_reason=post_edit_info.edit_reason,
+            )
+
+    except Exception as e:
+        print(f"Error in update_post: {type(e).__name__}: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Failed to fetch posts: {str(e)}")
+
+
+def delete_post(user_id, c_id, p_id):
+    try:
+        with get_db() as db:
+            post = (
+                db.query(Post).filter(Post.course_id == c_id, Post.id == p_id).first()
+            )
+            if not post:
+                raise HTTPException(status_code=404, detail="Post not found")
+
+            db.delete(post)
+            db.commit()
+            return post
+    except Exception as e:
+        print(f"Error in get_post: {type(e).__name__}: {str(e)}")
         raise HTTPException(status_code=500, detail=f"Failed to fetch posts: {str(e)}")
--- backend/routers/routes/posts.py
+++ backend/routers/routes/posts.py
@@ -2,9 +2,11 @@ from controllers import post_controller
 from fastapi import APIRouter, HTTPException, Request
 from models.schemas.general_schema import GeneralResponse
 from models.schemas.post_schema import (
+    CreatePostEditRequest,
     CreatePostRequest,
     GetPostResponse,
     GetPostsResponse,
+    PostResponse,
 )
 
 post_router = APIRouter()
@@ -25,7 +27,59 @@ async def get_posts(c_id: str):
     return {"posts": posts}
 
 
-# @post_router.get("/{post_id}", response_model=GetPostResponse)
-# async def get_post(post_id: str):
-#     post = post_controller.get_post(post_id)
+"""
+Missing Endpoints:
+- Get singlar post 
+    TODO
+    - Get edit history
+    - Get event history
+- Edit post DONE
+- Delete post DONE
+- View post TODO
+- Like post TODO
+- Get post metadata TODO
+
+- Missing DB
+    - post_impressions materialized view TODO
+"""
+
+
+# @post_router.get("/{p_id}", response_model=GetPostResponse)
+# async def get_post(c_id: str, p_id: str):
+#     post = post_controller.get_post(c_id, p_id)
+#     return {"post": post, "edits": [None], "events": [None]}
+
+
+@post_router.patch("", response_model=GeneralResponse)
+async def update_post(c_id: str, post_edit_info: CreatePostEditRequest):
+    post_controller.update_post(post_edit_info, c_id)
+
+    return {"msg": "Post edited successfully"}
+
+
+@post_router.delete("/{post_id}", response_model=GeneralResponse)
+async def delete_post(post_id: str, c_id: str, request: Request):
+    deletion_user_id = request.state.user_id
+    post = post_controller.delete_post(deletion_user_id, c_id, post_id)
+
+    return post
+
+
+# @post_router.put("/{post_id}/view")
+# async def view_post(post_id: str, request: Request):
+#     user_id = request.state.user_id
+#     post = post_controller.view_post(user_id, post_id)
+#     return post
+
+
+# @post_router.post("/{post_id}/like")
+# async def like_post(post_id: str, request: Request):
+#     user_id = request.state.user_id
+#     post = post_controller.like_post(user_id, post_id)
+#     return post
+
+
+# @post_router.get("/{post_id}/metadata", response_model=PostMetadata)
+# async def get_post_metadata(post_id: str):
+#     post = post_controller.get_post_metadata(post_id)
 #     return post
