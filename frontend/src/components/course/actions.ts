72bd307e404f074f1f105b5dabcd440fe0968e18
Armin Talaie <talaiearmin78@gmail.com>
2025-01-06 22:26:17 -0800

    railway issues

--- frontend/src/components/course/actions.ts
+++ frontend/src/components/course/actions.ts
@@ -1,8 +1,10 @@
+"use server";
+
 import { createClient } from "@/utils/supabase/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
-export function signOut() {
+export async function signOut() {
   const supabase = createClient();
   supabase.auth.signOut();
   revalidatePath("/", "layout");
