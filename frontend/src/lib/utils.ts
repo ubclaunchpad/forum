a918d35b03692484d306523cd82158d15e7221be
malcolm <malcolmmackenzie20@gmail.com>
2025-01-24 18:36:07 -0800

    linting

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -18,20 +18,20 @@ export function isPendingId(id: string): boolean {
 
 
 export function hexToHSL(hex: string) {
-  let r = parseInt(hex.substring(1, 3), 16) / 255;
-  let g = parseInt(hex.substring(3, 5), 16) / 255;
-  let b = parseInt(hex.substring(5, 7), 16) / 255;
+  const r = parseInt(hex.substring(1, 3), 16) / 255;
+  const g = parseInt(hex.substring(3, 5), 16) / 255;
+  const b = parseInt(hex.substring(5, 7), 16) / 255;
   
-  let max = Math.max(r, g, b);
-  let min = Math.min(r, g, b);
+  const max = Math.max(r, g, b);
+  const min = Math.min(r, g, b);
   let h = (max + min) / 2;
   let s = (max + min) / 2;
-  let l = (max + min) / 2;
+  const l = (max + min) / 2;
 
   if (max === min) {
     h = s = 0; // achromatic
   } else {
-    let d = max - min;
+    const d = max - min;
     s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
     switch (max) {
       case r: h = (g - b) / d + (g < b ? 6 : 0); break;
