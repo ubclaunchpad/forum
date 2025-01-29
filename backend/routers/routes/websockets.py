405e88477b5f593a5be2a4dabcc056ce5c42ef3d
Boris Wang <boris.wang117@gmail.com>
2025-01-28 18:49:39 -0800

    Fix merge conflicts

--- backend/routers/routes/websockets.py
+++ backend/routers/routes/websockets.py
@@ -4,14 +4,37 @@ from controllers import websocket_controller
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from models.schemas import message_schema
 
-web_router = APIRouter()
 
-@web_router.websocket("/ws/chat/{room_id}")
-async def chat_room(websocket: WebSocket, room_id: int):
-    """Handle WebSocket connections for a chat room."""
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        websocket_controller.sendMessage(data)
-        await websocket.send_text(f"Message text was: {data}")
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+web_router = APIRouter()
+manager = ConnectionManager()
+
+@web_router.websocket("/ws/chat/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
         
