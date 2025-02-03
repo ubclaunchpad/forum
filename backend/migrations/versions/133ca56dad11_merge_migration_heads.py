b2ec06e85d9240adcf4c73b0ca5fd0e2160e2895
Caeleb Koharjo <57655452+caelebk@users.noreply.github.com>
2025-02-02 19:24:20 -0800

    Tags API and migrations (#237)
    * migrations for roles and tags
    
    * migration works just update head
    
    * minor db model changes for semantics and due to errors
    
    * roles init commit
    
    * implemented several more apis
    
    * updated migrations to remove roles, commented out roles db and created new migration for only tags
    
    * init first get tag for roles
    
    * imports
    
    * comment out role code
    
    * commit to fix migration head
    
    * added post_tag and doc_tags tables
    
    * place migrations on top of new head
    
    * updated foreign key for post ID from INT to UUID (to match the changed type from merge)
    
    * added back queryhistory model
    
    * brought back get_all_tags from merge
    
    * comment out more courserole stuff
    
    * create tag endpoint
    
    * added general crud for tags
    
    * renamed tags specific to courses, course tags
    
    * added post_tags
    
    * fixed all errors with migration
    
    * minor refactor for post tags
    
    * added document tagging
    
    * lots of cleanup on db side
    
    * attempt: fixed dup fkey genereation
    
    * ignoring pylance issues for old migration issues
    
    * formatting
    
    * vs code exclude ruff cache
    
    * updated to grab authenticated user for created_by field
    
    * removed commented out roles
    
    * updated migrations, removed more role stuff
    
    * clean up + include pydantic schema
    
    * removed role imports
    
    * remove optional for course_id
    
    * updated migration head after pulling cleanup
    
    * kept enum model and fixed small insert error with post tag + doc tag
    
    * merge migrations from dev
    
    * merge heads for db
    
    * formatting
    
    * fixed type issues
    
    ---------
    
    Co-authored-by: harshamin <hamin04@students.cs.ubc.ca>
    Co-authored-by: Armin Talaie <talaiearmin78@gmail.com>
    Co-authored-by: Arman Moztarzadeh <armanmoztar@outlook.com>

--- backend/migrations/versions/133ca56dad11_merge_migration_heads.py
+++ backend/migrations/versions/133ca56dad11_merge_migration_heads.py
@@ -1,8 +1,8 @@
-"""empty message
+"""Merge migration heads
 
-Revision ID: dc19234cdc67
-Revises: 07f9fdd97141, 7a062c24ebe7
-Create Date: 2025-02-01 20:04:13.218271
+Revision ID: 133ca56dad11
+Revises: 07f9fdd97141, afad06f37d8f
+Create Date: 2025-02-01 16:35:38.253397
 
 """
 
@@ -13,8 +13,8 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = "dc19234cdc67"
-down_revision: Union[str, None] = ("07f9fdd97141", "7a062c24ebe7")  # type: ignore
+revision: str = "133ca56dad11"
+down_revision: Union[str, None] = ("07f9fdd97141", "afad06f37d8f")  # type: ignore
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
