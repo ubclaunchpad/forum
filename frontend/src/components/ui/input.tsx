f75488a54989a895de2d3fa0c7566f908ac9ff2d
Armin Talaie <talaiearmin78@gmail.com>
2025-02-06 01:51:59 -0800

    Updated Settings Page (#276)
    * better routing
    
    * formatting
    
    * migrated settings mgmt state to zustand + enhanced settings page
    
    * replaced coursecontext with coursestore
    
    * theme tuning

--- frontend/src/components/ui/input.tsx
+++ frontend/src/components/ui/input.tsx
@@ -10,7 +10,7 @@ const Input = React.forwardRef<
     <input
       type={type}
       className={cn(
-        "flex h-9 w-full rounded-md border border-input bg-background px-3 py-0 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        "flex h-9 w-full rounded-md border border-input bg-background px-3 py-0 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-200 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
         className,
       )}
       ref={ref}
