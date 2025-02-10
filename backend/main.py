20da9fac25cbe86b9a50f39904cfafd5e70f1ef3
Arman Moztarzadeh <armanmoztar@outlook.com>
2025-02-09 19:48:48 -0800

    Revert "Simple chat service" (#284)
    Revert "Simple chat service (#281)"
    
    This reverts commit 7c01ba869a8eb1f41d2d40d660e989e03b60be35.

--- backend/main.py
+++ backend/main.py
@@ -7,10 +7,8 @@ from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from models.db import get_db
 from routers.middleware.auth import AuthMiddleware
-from routers.routes.chat import chat_router
 from routers.routes.courses import course_router
 from routers.routes.documents import document_router
-from routers.routes.invites import invite_router
 from routers.routes.posts import post_router
 from routers.routes.query_history import query_history_router
 from routers.routes.users import user_router
@@ -34,7 +32,6 @@ app = FastAPI(dependencies=[Depends(get_db)])
 
 app.include_router(course_router, tags=["Courses"], prefix="/courses")
 app.include_router(user_router, tags=["Users"], prefix="/users")
-app.include_router(chat_router, prefix="/channels")
 app.include_router(invite_router, tags=["Invites"], prefix="/invites")
 course_router.include_router(post_router, tags=["Posts"], prefix="/{c_id}/posts")
 course_router.include_router(
