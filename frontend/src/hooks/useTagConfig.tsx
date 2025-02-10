9a9f7e7a440256b2e9e5ac118a9bb077c694e751
Armin Talaie <talaiearmin78@gmail.com>
2025-02-09 23:39:48 -0800

    Tagui (#277)
    * better routing
    
    * formatting
    
    * migrated settings mgmt state to zustand + enhanced settings page
    
    * replaced coursecontext with coursestore
    
    * theme tuning
    
    * Landing Page Initialization [V1] (#259)
    
    * init landing page
    
    * fix styling for mobile
    
    * switch to light theme
    
    * Include type field for issue templates (#261)
    
    * Add type field to issue templates
    
    * Update feature issue tempalte
    
    * small change
    
    * Include related issues for feature
    
    * re-order items in header
    
    * fix
    
    * fix button styling
    
    * added tags under the forum sidebar
    
    * adding tag creation
    
    * formatting + merge
    
    * updated tags sidebar
    
    ---------
    
    Co-authored-by: Shreyas Goyal <70737064+xenshreyas@users.noreply.github.com>
    Co-authored-by: Arman Moztarzadeh <armanmoztar@outlook.com>
    Co-authored-by: Shreyas Goyal <xenshreyas@gmail.com>

---
+++ frontend/src/hooks/useTagConfig.tsx
@@ -0,0 +1,66 @@
+import { userContext } from "@/contexts/userContext";
+import { useCourseStore } from "@/providers/courseStoreProvider";
+import { getApiUrl } from "@/utils/helpers";
+import { useContext, useState } from "react";
+import { useToast } from "./use-toast";
+import { Tag } from "@/lib/types/tags";
+
+export default function useTagsConfig() {
+  const { toast } = useToast();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+  const { token, user } = useContext(userContext);
+  const course = useCourseStore((state) => state.course);
+  const addTag = useCourseStore((state) => state.addTag);
+
+  async function addTagRequest({
+    tagToAdd,
+  }: {
+    tagToAdd: Pick<Tag, "name" | "visibility">;
+  }) {
+    try {
+      addTag({
+        course_id: course.id,
+        created_by: user.id,
+        id: "temp-" + tagToAdd.name,
+        ...tagToAdd,
+        properties: null,
+        parent_tag_id: null,
+      });
+      const response = await fetch(`${getApiUrl()}/courses/${course.id}/tags`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          ...tagToAdd,
+          properties: null,
+          parent_tag_id: null,
+        }),
+      });
+
+      if (!response.ok) throw new Error("Failed to update appearance");
+
+      //   const res = await response.json();
+
+      fetch(`/api/revalidate?tag=course-${course.id}-tags`);
+    } catch (error) {
+      console.error(error);
+      setError(error as Error);
+      toast({
+        title: "Error",
+        description: "Failed to add tag",
+        variant: "destructive",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return {
+    isLoading,
+    error,
+    addTagRequest,
+  };
+}
