f15648d6253346d4996c96cd064989fb63d5399a
Arman Moztarzadeh <armanmoztar@outlook.com>
2025-02-22 15:12:11 -0800

    new ui redesign for 404 page

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -8,21 +8,20 @@ export default function NotFound() {
   const router = useRouter();
 
   return (
-    <div className="min-h-dvh w-full justify-center bg-neutral-100 flex items-center overflow-hidden">
-      <Card className="w-full max-w-lg min-h-[450px] px-12 py-3 flex flex-col bg-neutral-50 rounded-xl [&_label]:pl-1">
+    <div className="min-h-dvh w-full justify-center bg-neutral-100 flex items-center overflow-hidden px-4 sm:px-0">
+      <Card className="w-full max-w-lg min-h-[325px] text-center py-3 flex flex-col bg-neutral-50 rounded-xl">
         <CardHeader className="pl-0">
-          <h2 className="text-2xl pl-0 pb-2">Page Not Found</h2>
+          <h2 className="text-2xl pl-0 pb-1">Page Not Found</h2>
         </CardHeader>
-        <CardContent className="p-2 flex flex-1 w-full flex-col items-center gap-4">
+        <CardContent className="p-1 flex flex-1 w-full flex-col items-center gap-4">
           <div className="flex-1 flex flex-col items-center justify-center gap-4">
-            <button
-              onClick={() => router.back()}
-              className="p-2 hover:bg-neutral-800 px-4 font-medium  text-center w-40 bg-black text-white rounded-full"
-            >
-              Go Back
-            </button>
+            <h2 className="text-primary-800 text-3xl font-semibold">404</h2>
+            <h3 className="text-center">
+              Sorry, the page you are looking for doesn't exist. If you think
+              something is broken, please let us know.
+            </h3>
             <Link
-              className="p-2 hover:bg-neutral-800 hover:text-white  px-4 font-medium text-center text-neutral-950 w-40 border bg-neutral-50 border-black text-white rounded-full"
+              className="h-10 px-6 sm:h-11 sm:px-8 bg-primary-600 pt-2 hover:bg-primary-700 text-white rounded-lg"
               href="/"
             >
               Return Home
