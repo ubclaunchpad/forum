f7af10cf40890de1afbf6e9e57b5de27218ed239
Armin Talaie <talaiearmin78@gmail.com>
2025-02-27 00:07:09 -0800

    Google auth (#323)
    * Add draft
    
    * Add button for signup
    
    * google auth flow to work with invite and profile creation
    
    * auth providers under toggle only shown if dev env supports
    
    * fix build issue
    
    ---------
    
    Co-authored-by: Arman Moztarzadeh <armanmoztar@outlook.com>
    Co-authored-by: Armin T <arminoacid@Scooby-Did.local>

--- frontend/src/app/api/users/finish-setup/route.ts
+++ frontend/src/app/api/users/finish-setup/route.ts
@@ -1,10 +1,9 @@
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
+import { createClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   try {
-    const supabase = createRouteHandlerClient({ cookies });
+    const supabase = await createClient();
     const {
       data: { user },
     } = await supabase.auth.getUser();
