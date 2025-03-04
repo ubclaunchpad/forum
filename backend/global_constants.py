e185782fb207579d09b5a73c8e3de9fc9ee4bdd1
Ray Zhou <rayzhou4@gmail.com>
2025-03-04 10:44:52 -0800

    HOTFIX: adding global_constants.py

---
+++ backend/global_constants.py
@@ -0,0 +1,14 @@
+CONTENTTYPEMAP = {
+    "pdf": "application/pdf",
+    "txt": "text/plain",
+    "md": "text/markdown",
+    "png": "image/png",
+    "jpg": "image/jpeg",
+    "jpeg": "image/jpeg",
+    "mp3": "audio/mpeg",
+    "mp4": "video/mp4",
+    "ppt": "application/vnd.ms-powerpoint",
+    "pptx": "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+    "doc": "application/msword",
+    "docx": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+}
