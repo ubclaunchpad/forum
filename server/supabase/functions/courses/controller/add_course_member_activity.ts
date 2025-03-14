22c30517f67fbef9f93c87e70d01f4a263257570
Victor Akita Kang <victors1.kang@gmail.com>
2025-03-14 16:33:37 -0700

    Refactoring of add_course_member and adding tests

--- server/supabase/functions/courses/controller/add_course_member_activity.ts
+++ server/supabase/functions/courses/controller/add_course_member_activity.ts
@@ -1,20 +1,13 @@
-import { supa } from "../../_shared/db.ts"; 
+import { supa } from "../_shared/db.ts"; 
+import { Course } from "../../shared/schema/course.ts";
+import { NotFoundError, PermissionError } from '../../_shared/errors.ts';
 
 export async function addUserToCourse(
   course_id: string,
   user_id: string,
   role?: any // TODO: @victor: fix this
 ): Promise<boolean> { // TODO: @victor: probably good for this function to use one of error handling or return false/true instead of a mix of both
-    const { data: course, error: courseError } = await supa.from("courses")
-        .select("*").eq("id", course_id).single();
-
-    if (courseError || !course) {
-        throw new Error("Course not found");
-    }
-
-    if (course.access === "private") {
-        throw new Error("Cannot join a private course");
-    }
+    const course = getCourse(course_id);
 
     const { data: user, error: userError } = await supa.from("profiles").select("*").eq("id", user_id).single();
 
@@ -34,6 +27,43 @@ export async function addUserToCourse(
     }
 
     // TODO: @victor: check shared/schema/course.ts for notes on how to get this is a more type safe way
+    const roleData = getRole(role);
+
+    const { error: joinError } = await supa
+        .from("user_courses")
+        .insert([{ 
+            course_id: course_id, 
+            user_id: user_id, 
+            role_id: roleData.id 
+        }]);
+
+    if (joinError) {
+        throw new Error(`Failed to add user to course: ${joinError.message}`);
+    }
+
+    return true;
+}
+
+function getCourse(course_id: string): Course { 
+    const { data: course, error: courseError } = await supa.from("courses")
+        .select("*").eq("id", course_id).single();
+
+    if (courseError) {
+        throw new Error(`Database error when retrieving course with id $(course_id): $() `);
+    }
+
+    if (!course) {
+        throw new NotFoundError(`Course with id $(course_id) not found`);
+    }
+
+    if (course.access === "private") {
+        throw new PermissionError("Cannot join a private course");
+    }
+    
+    return course as Course;
+}
+
+function getRole(role: string) {
     if (!role) {
         role = "student"; // use student by default
     }
@@ -43,17 +73,13 @@ export async function addUserToCourse(
         .eq("name", role)
         .single();
 
-    if (roleError || !roleData) {
-        throw new Error("Role not found: " + (roleError?.message || "No data returned"));
+    if (roleError) {
+        throw new Error("Database error ")
     }
 
-    const { error: joinError } = await supa
-        .from("user_courses")
-        .insert([{ course_id: course_id, user_id: user_id, role_id: roleData.id }]);
-
-    if (joinError) {
-        throw new Error(`Failed to add user to course: ${joinError.message}`);
+    if (!roleData) {
+        throw new NotFoundError("Role not found: " + (roleError?.message || "No data returned"));
     }
 
-    return true;
+    return roleData;
 }
