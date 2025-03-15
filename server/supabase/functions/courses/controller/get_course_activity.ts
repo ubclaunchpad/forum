435394d0cc578dab3c42822dccd907fcd00baffa
Ray Zhou <rayzhou4@gmail.com>
2025-03-14 17:05:03 -0700

    get courses by user id

--- server/supabase/functions/courses/controller/get_course_activity.ts
+++ server/supabase/functions/courses/controller/get_course_activity.ts
@@ -4,14 +4,26 @@ import {
 } from "@shared/schema/course.ts";
 import { NotFoundError } from "../../_shared/errors.ts";
 
+// get course by course ID
 export async function getCourse(course_id: string): Promise<Course> {
     const { data: course, error: _ } = await supa.from("courses")
         .select("*").eq("id", course_id).single();
 
-  if (!course) {
-    throw new NotFoundError("Course not found");
-  }
-  
-  return course as Course;
+    if (!course) {
+        throw new NotFoundError("Course not found");
+    }
+
+    return course as Course;
 }
 
+// get courses by user ID
+export async function getCourses(user_id: string): Promise<[]Course > {
+    const { data: courses, error: _ } = await supa.from("courses")
+        .select("*").eq("id", user_id).all();
+
+    if(len(courses) === 0) {
+        throw new NotFoundError("Courses not found");
+    };
+
+    return courses as []Course;
+}
